dc89b7f9e231e64bb4d6b29b4132c575ae94dc9f
photonnn <wasjiri@gmail.com>
2022-08-24 13:15:23 +0200

    Finish image slider, works

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -14,49 +14,78 @@ content.addEventListener('click', () => {
 // Find a way to do this automatically!!
 const arr = ['img_1.png', 'img_2.png', 'img_3.png', 'img_4.png'];
 
-function move(n) {
-  // const img = [...document.querySelectorAll('.carousel img')][0];
+function getNumber(direction) {
   const images = [...document.querySelectorAll('.carousel img')];
-
-  const next = document.querySelector('.next');
   let number;
-  images.forEach((element, i) => {
-    if (element === next) {
-      number = i;
-    } // else if (element === leftNext) {
-    // leftNumber = i;
-    // }
+  images.forEach((element, index) => {
+    if (element === direction) {
+      number = index;
+    }
   });
 
-  // left
-  if (number === 0 && n === -1) number = arr.length;
+  return number;
+}
 
+function setupTransform(nextNumber) {
   const imgContainer = document.querySelector('.images');
-  if (n === 1) {
-    imgContainer.style.transform = `translate(calc(-800px*${number}))`;
-  } else if (number === 1) {
-    imgContainer.style.transform = `translate(calc(-800px*${arr.length - number}))`;
-  } else if (number === 4) {
-    // 1) make seperate next for left and right button
-    // 2) get current translation and change based on that
-    // imgContainer.style.transform = `translate(calc(-1600px))`;
+  imgContainer.style.transform = `translate(calc(-800px*${nextNumber}))`;
+}
+
+function setupNextAndPrev(number, direction) {
+  const images = [...document.querySelectorAll('.carousel img')];
+  const newNumber = ((number + 2) % 4);
+  console.log(newNumber);
+  if (direction === 'next') {
+    const prevImg = document.querySelector('.prev');
+    prevImg.classList.remove('prev');
+    images[newNumber].classList.add('prev');
+  } else {
+    const nextImg = document.querySelector('.next');
+    nextImg.classList.remove('next');
+    images[newNumber].classList.add('next');
   }
-  // right
-  if (number + 1 === arr.length && n === 1) number = -1;
-  next.classList.remove('next');
-  images[number + n].classList.add('next');
+}
+
+function next() {
+  const images = [...document.querySelectorAll('.carousel img')];
+
+  const nextImg = document.querySelector('.next');
+
+  let nextNumber = getNumber(nextImg);
+
+  setupTransform(nextNumber);
+
+  if (nextNumber + 1 === arr.length) nextNumber = -1;
+  nextImg.classList.remove('next');
+  images[nextNumber + 1].classList.add('next');
+  setupNextAndPrev(nextNumber + 1, 'next');
+}
+
+function previous() {
+  const images = [...document.querySelectorAll('.carousel img')];
+
+  const prevImg = document.querySelector('.prev');
+
+  let prevNumber = getNumber(prevImg);
+
+  setupTransform(prevNumber);
+
+  if (prevNumber === 0) prevNumber = arr.length;
+  prevImg.classList.remove('prev');
+  images[prevNumber - 1].classList.add('prev');
+  setupNextAndPrev(prevNumber - 1, 'prev');
 }
 
 const right = document.querySelector('.right');
 right.addEventListener('click', () => {
-  move(1);
+  next();
 });
 
 const left = document.querySelector('.left');
 left.addEventListener('click', () => {
-  move(-1);
+  previous();
 });
-/*
+
 setInterval(() => {
-  move(1);
-}, 5000); */
+  next();
+}, 5000);
