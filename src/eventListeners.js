915f5948c5012af41b7b0186d052933bae4737fa
photonnn <wasjiri@gmail.com>
2022-08-24 13:29:27 +0200

    Add function to reset interval

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -12,6 +12,7 @@ content.addEventListener('click', () => {
 });
 
 // Find a way to do this automatically!!
+
 const arr = ['img_1.png', 'img_2.png', 'img_3.png', 'img_4.png'];
 
 function getNumber(direction) {
@@ -26,23 +27,32 @@ function getNumber(direction) {
   return number;
 }
 
+// we are transforming the container rather than use background-image, so we can
+// get cool transitions on the images
 function setupTransform(nextNumber) {
   const imgContainer = document.querySelector('.images');
   imgContainer.style.transform = `translate(calc(-800px*${nextNumber}))`;
 }
 
-function setupNextAndPrev(number, direction) {
+// this is seperate from the one in function, though with little changes it may
+// be further optimized for readability, because this is taken into account
+// after the next prev or next is assigned after the user clicks the button
+// therefore the one in the next/previous has to taken in account +1/-1
+function assignNextOrPrev(number, direction) {
   const images = [...document.querySelectorAll('.carousel img')];
+  const img = document.querySelector(`.${direction}`);
+  img.classList.remove(`${direction}`);
+  images[number].classList.add(`${direction}`);
+}
+
+// prev and next always have to be 2 units away from each other
+// therefore when one changes so must the other one
+function setupNextAndPrev(number, direction) {
   const newNumber = ((number + 2) % 4);
-  console.log(newNumber);
   if (direction === 'next') {
-    const prevImg = document.querySelector('.prev');
-    prevImg.classList.remove('prev');
-    images[newNumber].classList.add('prev');
+    assignNextOrPrev(newNumber, 'prev');
   } else {
-    const nextImg = document.querySelector('.next');
-    nextImg.classList.remove('next');
-    images[newNumber].classList.add('next');
+    assignNextOrPrev(newNumber, 'next');
   }
 }
 
@@ -76,16 +86,27 @@ function previous() {
   setupNextAndPrev(prevNumber - 1, 'prev');
 }
 
+let interval = setInterval(() => {
+  next();
+}, 5000);
+
+// this is done once user clicks a button, so it does not obstruct their
+// experience with annoying sliding
+function resetInterval() {
+  clearInterval(interval);
+  interval = setInterval(() => {
+    next();
+  }, 5000);
+}
+
 const right = document.querySelector('.right');
 right.addEventListener('click', () => {
+  resetInterval();
   next();
 });
 
 const left = document.querySelector('.left');
 left.addEventListener('click', () => {
+  resetInterval();
   previous();
 });
-
-setInterval(() => {
-  next();
-}, 5000);
